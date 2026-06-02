7f14a1af4d1daca613033798aca44967ec89ff0d
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-02 14:19:08 +0200

    feat: add Tensor class definition

--- include/axon/tensor.hpp
+++ include/axon/tensor.hpp
@@ -5,4 +5,38 @@
 #ifndef AXON_TENSOR_HPP
 #define AXON_TENSOR_HPP
 
+#include <vector>
+#include <numeric>
+
+namespace axon {
+     class Tensor;
+}
+
+
+class axon::Tensor {
+private:
+     std::shared_ptr<std::vector<float> > data_;
+     std::vector<size_t> shape_;
+     std::vector<int64_t> stride_;
+
+     size_t offset_;
+
+public:
+     Tensor(const std::vector<size_t> &shape, const std::vector<float> &data);
+
+     //TODO [] operator
+
+     const std::vector<size_t> &shape() const { return shape_; };
+     const std::vector<int64_t> &stride() const { return stride_; };
+     size_t offset() const { return offset_; };
+
+     template<typename... Idx>
+     float at(Idx... indices) const;
+
+     size_t num_dim() const { return shape_.size(); };
+
+     size_t num_elements() const {
+          return std::accumulate(shape_.begin(), shape_.end(), 1, std::multiplies<size_t>());
+     };
+};
 #endif //AXON_TENSOR_HPP
