abdaa33fbdab5dc29b8acca5c2ccb5d8b96aad4d
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-03 12:46:02 +0200

    test: tests for Tensor implementation

---
+++ tests/tensor_test.cpp
@@ -0,0 +1,25 @@
+#include <catch2/catch_test_macros.hpp>
+#include "axon/tensor.hpp"
+TEST_CASE("Tensor implementation", "[Tensor]") {
+  const axon::Tensor t({2, 3}, {1, 2, 3, 4, 5, 6});
+
+  REQUIRE(t.num_elements() == 6);
+  REQUIRE(t.num_dim() == 2);
+
+  REQUIRE(t.shape().at(0) == 2);
+  REQUIRE(t.shape().at(1) == 3);
+
+  REQUIRE(t.stride().size()== 2);
+  REQUIRE(t.stride().at(0) == 3);
+  REQUIRE(t.stride().at(1) == 1);
+
+  REQUIRE(t.at({0,0})== 1.0f);
+  REQUIRE(t.at({0,1})== 2.0f);
+  REQUIRE(t.at({0,2})== 3.0f);
+  REQUIRE(t.at({1,0})== 4.0f);
+  REQUIRE(t.at({1,1})== 5.0f);
+  REQUIRE(t.at({1,2})== 6.0f);
+
+  REQUIRE_THROWS_AS(t.at({5, 0}), std::out_of_range);
+  REQUIRE_THROWS_AS(t.at({0}), std::out_of_range);
+}
