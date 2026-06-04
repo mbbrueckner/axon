24e8840ebeebf76468977615c80e54b076d3ccf4
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-04 15:14:11 +0200

    refactor: update Tensor to use int64_t for shape and index handling

--- include/axon/tensor.hpp
+++ include/axon/tensor.hpp
@@ -4,42 +4,41 @@
 
 #pragma once
 
-#include <vector>
-#include <numeric>
 #include <memory>
+#include <numeric>
+#include <vector>
 
 namespace axon {
-     class Tensor;
+class Tensor;
 }
 
-
 class axon::Tensor {
-private:
-     std::vector<size_t> shape_;
-     std::vector<int64_t> stride_;
-     size_t offset_;
-     std::shared_ptr<std::vector<float> > data_;
+ private:
+  std::vector<int64_t> shape_;
+  std::vector<int64_t> stride_;
+  size_t offset_;
+  std::shared_ptr<std::vector<float>> data_;
 
 
-     void calculate_strides();
+  void calculate_strides();
 
-public:
-     Tensor(const std::vector<size_t> &shape, const std::vector<float> &data);
+ public:
+  Tensor(const std::vector<int64_t>& shape, const std::vector<float>& data);
 
-     explicit Tensor(const std::vector<size_t> &shape);
+  explicit Tensor(const std::vector<int64_t>& shape);
 
-     //TODO [] operator
+  // TODO [] operator
 
-     [[nodiscard]] const std::vector<size_t> &shape() const { return shape_; };
-     [[nodiscard]] const std::vector<int64_t> &stride() const { return stride_; };
-     [[nodiscard]] size_t offset() const { return offset_; };
+  [[nodiscard]] const std::vector<int64_t>& shape() const { return shape_; };
+  [[nodiscard]] const std::vector<int64_t>& stride() const { return stride_; };
+  [[nodiscard]] size_t offset() const { return offset_; };
 
-     [[nodiscard]] size_t num_dim() const { return shape_.size(); };
+  [[nodiscard]] size_t num_dim() const { return shape_.size(); };
 
-     [[nodiscard]] size_t num_elements() const {
-          return std::accumulate(shape_.begin(), shape_.end(), 1, std::multiplies<size_t>());
-     };
+  [[nodiscard]] size_t num_elements() const {
+    return std::accumulate(
+        shape_.begin(), shape_.end(), 1, std::multiplies<>());
+  };
 
-     [[nodiscard]] float at(std::initializer_list<size_t> indices) const;
+  [[nodiscard]] float at(std::initializer_list<int64_t> indices) const;
 };
-
--- src/tensor.cpp
+++ src/tensor.cpp
@@ -1,51 +1,62 @@
 #include "axon/tensor.hpp"
-#include <stdexcept>
-#include <format>
 
+#include <format>
+#include <stdexcept>
+#include <utility>
 
 void axon::Tensor::calculate_strides() {
   const size_t dim = shape_.size();
   stride_.resize(dim, 1);
 
-  if (dim < 2) return; // check if tensor or is 0D/1D -> no stride needed
+axon::Tensor::Tensor(const std::vector<int64_t>& shape,
+                     const std::vector<float>& data)
+    : shape_(shape),
+      offset_(0),
+      data_(std::make_shared<std::vector<float>>(data)) {
+  calculate_strides();
+}
 
-  for (int i = dim - 2; i >= 0; i--) {
-    stride_[i] = shape_[i + 1] * stride_[i + 1]; // stride[i] = shape[i+1] * stride[i+1]; stride[dim-1] = 1
+axon::Tensor::Tensor(const std::vector<int64_t>& shape)
+    : shape_(shape),
+      offset_(0),
+      data_(std::make_shared<std::vector<float>>(std::accumulate(
+          shape_.begin(), shape_.end(), 1, std::multiplies<>()))) {
+  calculate_strides();
+}
+
+void axon::Tensor::calculate_strides() {
+  const size_t dim = shape_.size();
+  stride_.resize(dim, 1);
+
+  if (dim < 2) return;  // check if tensor or is 0D/1D -> no stride needed
+
+  for (int64_t i = static_cast<int64_t>(dim) - 2; i >= 0; i--) {
+    // stride[i] = shape[i+1] * stride[i+1]; stride[dim-1] = 1
+    stride_[i] = shape_[i + 1] * stride_[i + 1];
   }
 }
 
-axon::Tensor::Tensor(const std::vector<size_t> &shape, const std::vector<float> &data)
-  : shape_(shape),
-    offset_(0),
-    data_(std::make_shared<std::vector<float> >(data)) {
-  calculate_strides();
-}
-
-axon::Tensor::Tensor(const std::vector<size_t> &shape)
-  : shape_(shape),
-    offset_(0),
-    data_(std::make_shared<std::vector<float> >(
-      std::accumulate(shape_.begin(), shape_.end(), 1, std::multiplies<size_t>()))) {
-  calculate_strides();
-}
-
-float axon::Tensor::at(std::initializer_list<size_t> indices) const {
+float axon::Tensor::at(std::initializer_list<int64_t> indices) const {
   const size_t num_dim = shape_.size();
   const size_t num_indices = indices.size();
 
   if (num_indices != num_dim) {
     throw std::out_of_range(
-      std::format("Number of indices does not match dimensions: got {} indices for a {}-dimensional tensor",
-                  num_indices,
-                  num_dim));
+        std::format("Number of indices does not match dimensions: got {} "
+                    "indices for a {}-dimensional tensor",
+                    num_indices,
+                    num_dim));
   }
 
-  size_t dim = 0;
+  int64_t dim = 0;
   size_t flat = offset_;
-  for (size_t idx: indices) {
+  for (int64_t idx : indices) {
     if (idx >= shape_[dim]) {
       throw std::out_of_range(
-        std::format("Index {} out of range for dimension {} with size {}", idx, dim, shape_[dim]));
+          std::format("Index {} out of range for dimension {} with size {}",
+                      idx,
+                      dim,
+                      shape_[dim]));
     }
 
     flat += idx * stride_[dim];
