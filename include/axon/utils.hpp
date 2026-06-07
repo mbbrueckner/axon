56e957112167d0a6a426c9657cdd1ce7796a2f9d
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-07 18:47:51 +0200

    docs: add Doxygen comments for utils

--- include/axon/utils.hpp
+++ include/axon/utils.hpp
@@ -1,14 +1,27 @@
-//
-// Created by Mika Brückner on 04.06.26.
-//
+/**
+ * @file utils.hpp
+ * @brief Small general-purpose helper utilities used across axon.
+ * @author Mika Brückner
+ * @date 2026-06-04
+ */
 
 #pragma once
 #include <string>
 #include <vector>
 
 namespace axon {
+/// @brief General-purpose helper utilities.
 namespace utils {
 
+/**
+ * @brief Formats a vector as a bracketed, comma-separated string.
+ *
+ * Produces output of the form @c "[a,b,c]"; an empty vector yields @c "[]".
+ *
+ * @tparam T Element type; must be supported by @c std::to_string.
+ * @param vec The vector to format.
+ * @return The string representation of @p vec.
+ */
 template <typename T>
 std::string vector_to_string(const std::vector<T>& vec) {
   std::string vec_string = "[";
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,8 @@
-//
-// Created by Mika Brückner on 04.06.26.
-//
+/**
+ * @file utils.cpp
+ * @brief Implementation unit for axon::utils helpers (header-only for now).
+ * @author Mika Brückner
+ * @date 2026-06-04
+ */
 
 #include "axon/utils.hpp"
