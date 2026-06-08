d474ae5788eccc26667c1d357ac3acdd056c9edc
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-08 12:33:35 +0200

    feat: add constants header with idx_t definition

---
+++ include/axon/constants.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file constants.hpp
+ * @brief Constants and type definitions within the axon:: namespace.
+ * @author Mika Brückner
+ * @date 2026-06-08
+ */
+
+#pragma once
+#include <cstdint>
+
+namespace axon {
+using idx_t = int64_t;
+}
