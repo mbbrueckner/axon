040eb2e3fc2328de11f7210a42513db47c8d6b03
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-13 12:42:27 +0200

    test: add unit tests for autograd sum and matmul backward operations

--- tests/autograd_test.cpp
+++ tests/autograd_test.cpp
@@ -63,3 +63,38 @@ TEST_CASE("Autograd divide backward", "[AutoGradDiv]") {
   REQUIRE(x.grad().at({0}) == Catch::Approx(1.0f / 3.0f));
   REQUIRE(y.grad().at({0}) == Catch::Approx(-2.0f / 9.0f));
 }
+
+TEST_CASE("Autograd sum backward", "[AutoGradSum]") {
+  axon::Tensor t({1.0f, 2.0f, 3.0f, 4.0f}, {2, 2});
+  t.requires_grad_(true);
+
+  axon::Tensor z = t.sum();
+  z.backward();
+
+  REQUIRE(t.grad().at({0, 0}) == 1.0f);
+  REQUIRE(t.grad().at({0, 1}) == 1.0f);
+  REQUIRE(t.grad().at({1, 0}) == 1.0f);
+  REQUIRE(t.grad().at({1, 1}) == 1.0f);
+}
+
+TEST_CASE("Autograd matmul backward", "[AutoGradMatMul]") {
+  axon::Tensor x({1.0f, 2.0f, 3.0f, 4.0f}, {2, 2});
+  axon::Tensor y({4.0f, 3.0f, 2.0f, 1.0f}, {2, 2});
+
+  x.requires_grad_(true);
+  y.requires_grad_(true);
+
+  axon::Tensor z = x.matmul(y);
+  axon::Tensor loss = z.sum();
+  loss.backward();
+
+  REQUIRE(x.grad().at({0, 0}) == Catch::Approx(7.0f));
+  REQUIRE(x.grad().at({0, 1}) == Catch::Approx(3.0f));
+  REQUIRE(x.grad().at({1, 0}) == Catch::Approx(7.0f));
+  REQUIRE(x.grad().at({1, 1}) == Catch::Approx(3.0f));
+
+  REQUIRE(y.grad().at({0, 0}) == Catch::Approx(4.0f));
+  REQUIRE(y.grad().at({0, 1}) == Catch::Approx(4.0f));
+  REQUIRE(y.grad().at({1, 0}) == Catch::Approx(6.0f));
+  REQUIRE(y.grad().at({1, 1}) == Catch::Approx(6.0f));
+}
