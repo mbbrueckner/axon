835ef426b30e1140457ca4ab0db1da3bd21608e8
Mika Brueckner <212235493+mbbrueckner@users.noreply.github.com>
2026-06-16 06:27:03 +0200

    fix: fix gradient computation in - operator

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -525,8 +525,8 @@ Tensor operator-(const Tensor& lhs, const Tensor& rhs) {
     meta->grad_fn_ = std::make_shared<GradFn>();
     meta->grad_fn_->backward =
         [lhs_meta, rhs_meta, lhs, rhs](const Tensor& grad_output) {
-          if (lhs_meta) *lhs_meta->grad -= grad_output;
-          if (rhs_meta) *rhs_meta->grad -= grad_output;
+          if (lhs_meta) *lhs_meta->grad += grad_output;
+          if (rhs_meta) *rhs_meta->grad += grad_output;
         };
     std::vector<std::shared_ptr<AutogradMeta>> inputs;
     if (lhs_meta) inputs.push_back(lhs_meta);
