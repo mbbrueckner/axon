efc3c0a19313b29ef96eef436ce360a1dc8210bd
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-17 10:54:44 +0200

    docs: add documentation for `Module` class

--- include/axon/module.hpp
+++ include/axon/module.hpp
@@ -1,16 +1,44 @@
-//
-// Created by Mika Brückner on 17.06.26.
-//
+/**
+ * @file module.hpp
+ * @brief Declaration of the axon::Module abstract base class, the common
+ *        interface for composable neural network building blocks.
+ * @author Mika Brückner
+ * @date 2026-06-17
+ */
 
 #pragma once
+#include <vector>
+
 #include "tensor.hpp"
 namespace axon {
 class Module;
 }
 
+/**
+ * @brief Abstract base class for all neural network modules.
+ *
+ * A Module represents a composable building block of a neural network, such as
+ * a layer, an activation function, or an entire model. Derived classes
+ * implement the forward pass and expose their trainable parameters.
+ */
 class axon::Module {
  public:
+  /**
+   * @brief Computes the forward pass of the module.
+   *
+   * @param input The input tensor.
+   * @return The result of applying the module to @p input.
+   */
   virtual Tensor forward(const Tensor& input) = 0;
+
+  /**
+   * @brief Collects the module's trainable parameters.
+   *
+   * @return A list of tensors representing the module's parameters, e.g. for
+   *         use by an optimizer.
+   */
   virtual std::vector<Tensor> parameters() = 0;
+
+  /// @brief Virtual destructor to allow safe polymorphic deletion.
   virtual ~Module() = default;
 };
