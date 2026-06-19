503bfd685c5224910a4ca03915ada537120445b6
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-19 16:18:49 +0200

    test: add unit tests for ReLU module

---
+++ tests/relu_test.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file relu_test.cpp
+ * @brief Unit tests for the axon::ReLU activation layer module.
+ * @author Mika Brückner
+ * @date 2026-06-19
+ */
+
+#include "../include/axon/relu.hpp"
+
+#include <cmath>
+
+#include "../include/axon/tensor.hpp"
+#include "catch2/catch_all.hpp"
+
+TEST_CASE("ReLU forward produces the expected output ", "[ReLU]") {
+  const std::vector<axon::idx_t> input_shape{4};
+  axon::ReLU layer{};
+
+  const axon::Tensor input =
+      axon::Tensor::from_data({-1.0f, 2.0f, -3.0f, 4.0f}, input_shape);
+
+  axon::Tensor output = layer.forward(input);
+
+  REQUIRE(output.shape() == input_shape);
+  REQUIRE(output.at({0}) == 0.0f);
+  REQUIRE(output.at({1}) == 2.0f);
+  REQUIRE(output.at({2}) == 0.0f);
+  REQUIRE(output.at({3}) == 4.0f);
+}
