1b45cab6e25c2c0fe6f70ac2be7c78f820be0ac7
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-26 14:33:09 +0200

    feat: add xor example

---
+++ examples/xor_demo.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Mika Brückner on 26.06.26.
+//
+
+#include <iostream>
+#include <ostream>
+
+#include "axon/functional.hpp"
+#include "axon/modules/linear.hpp"
+#include "axon/modules/relu.hpp"
+#include "axon/modules/sequential.hpp"
+#include "axon/optimizers/sgd.hpp"
+#include "axon/tensor.hpp"
+
+int main(int argc, char *argv[]) {
+  const axon::Tensor xor_input =
+      axon::Tensor::from_data({0, 0, 0, 1, 1, 0, 1, 1}, {4, 2});
+
+  const axon::Tensor xor_target = axon::Tensor::from_data({0, 1, 1, 0}, {4, 1});
+
+  std::vector<std::unique_ptr<axon::nn::Module>> modules;
+  modules.push_back(std::make_unique<axon::nn::Linear>(2, 4));
+  modules.push_back(std::make_unique<axon::nn::ReLU>());
+  modules.push_back(std::make_unique<axon::nn::Linear>(4, 1));
+  axon::nn::Sequential model(std::move(modules));
+
+  axon::optimizer::SGD optimizer(model.parameters());
+
+  for (int epoch = 0; epoch < 1000; epoch++) {
+    optimizer.zero_grad();
+    axon::Tensor output = model.forward(xor_input);
+    axon::Tensor loss = axon::mse_loss(output, xor_target);
+    loss.backward();
+    optimizer.step();
+    std::cout << "Epoch: " << epoch << "; loss:" << loss.item() << std::endl;
+  }
+}
